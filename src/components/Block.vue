367fd1a444c25c5ae5c86dac83a167a7b448224c
Fernando_Jose <lopez.fernando@clsu2.edu.ph>
2023-02-16 19:15:36 +0800

    vue

---
+++ src/components/Block.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <div v-if="showBlock" @click="stopTimer" class="mt-3 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Catch Me!</h5>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[ 'delay' ],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    }
+  },
+  mounted() {
+    console.log('mounted')
+    setTimeout(() =>{
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer(){
+      this.timer = setInterval(()=>{
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer(){
+      clearInterval(this.timer)
+      // console.log(this.reactionTime)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Your Reaction Time is {{ score }} millisecond</p>
+        <!-- <slot name="display"></slot> -->
+        <p class="text-4xl font-semibold text-teal-600 dark:text-white">{{ rank }}</p>
+    </div>
+</template>
+<script>
+export default {
+    props: [ 'score' ],
+    data(){
+        return{
+            rank: null,
+        }
+    },
+    mounted(){
+        if(this.score < 250 ){
+            this.rank = 'Ninja Fingers!'
+        }else if(this.score < 400){
+            this.rank = 'Rapid Reflexes'
+        }else{
+            this.rank = 'Snail Pecs...'
+        }
+    }
+}
+</script>
